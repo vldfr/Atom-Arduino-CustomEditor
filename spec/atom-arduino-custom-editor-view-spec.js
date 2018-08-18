5e578d1e4e99858b62a4c9fea26017667f1533fc
Vlad Fara <vlad.fara@gmail.com>
2018-08-18 15:15:34 +0300

    Generated Package

---
+++ spec/atom-arduino-custom-editor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomArduinoCustomEditorView from '../lib/atom-arduino-custom-editor-view';
+
+describe('AtomArduinoCustomEditorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
